424a5de0e02a775d18592e48f8d26a69080344ce
AloneSpace <jomcootle@gmail.com>
2020-11-04 00:13:03 +0700

    - Add inputPlayerName() - Add getter getGameWin() - Add getter getGamePlayed()

--- src/me/minesweeper/Player.java
+++ src/me/minesweeper/Player.java
@@ -1,19 +1,35 @@
 package me.minesweeper;
 
+import java.util.Scanner;
+
 public class Player {
 
     private String playerName;
+    private int gamePlayed;
+    private int gameWin;
     private boolean isWin;
 
-    public Player(String playerName) {
-        setPlayerName(playerName);
+    public Player() {
     }
 
     public String getPlayerName() {
         return playerName;
     }
 
-    public void setPlayerName(String playerName) {
-        this.playerName = playerName;
+    public void inputPlayerName(Scanner scanner) {
+        try {
+            System.out.print("Enter your name => ");
+            playerName = scanner.next();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public int getGameWin() {
+        return gameWin;
+    }
+
+    public int getGamePlayed() {
+        return gamePlayed;
     }
 }
