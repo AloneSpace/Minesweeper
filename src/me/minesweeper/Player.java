f5b151e786647f24c36e9276730edff624a640ce
AloneSpace <jomcootle@gmail.com>
2020-11-19 00:59:25 +0700

    - เพิ่ม method ใน Player Class เกี่ยวกับการ เพิ่มจำนวนครั้งที่เลือกไม่โดนระเบิด, เพิ่มจำนวนครั้งที่เลือกโดนระเบิด - จัดเรียงไฟล์ใหม่

--- src/me/minesweeper/Player.java
+++ src/me/minesweeper/Player.java
@@ -5,28 +5,8 @@ public class Player {
     private String playerName;
     private int gamePlayed;
     private int gameWin;
-
-    public Player() {
-        increaseGamePlayed();
-    }
-
-    /**
-     * @return String: ชื่อผู้เล่น
-     */
-    public String getPlayerName() {
-        return playerName;
-    }
-
-    public int getGameWin() {
-        return gameWin;
-    }
-
-    /**
-     * @return int: จำนวนรอบที่เราเล่นทั้งหมด
-     */
-    public int getGamePlayed() {
-        return gamePlayed;
-    }
+    private int pickup;
+    private int bombPickup;
 
     /**
      * เพื่อเพิ่มจำนวนรอบ เมื่อมีการเริ่มเล่นใหม่
@@ -36,9 +16,66 @@ public class Player {
     }
 
     /**
+     * เพื่อเพิ่มจำนวนรอบที่ชนะ
+     */
+    public void increaseGameWin() {
+        gameWin++;
+    }
+
+    public void increasePickup() {
+        pickup++;
+    }
+
+    public void increaseBombPickup() {
+        bombPickup++;
+    }
+
+    /**
      * @param playerName ใส่ชื่อผู้เล่น
      */
     public void setPlayerName(String playerName) {
         this.playerName = playerName;
+    }
+
+    /**
+     * @return จำนวนรอบทั้งหมดที่ผู้เล่นเลือก
+     */
+    public int getPickup() {
+        return pickup;
+    }
+
+    /**
+     * @return จำนวนรอบทั้งหมดที่ผู้เล่นเลือกแล้วโดนระเบิด
+     */
+    public int getBombPickup() {
+        return bombPickup;
+    }
+
+    /**
+     * @return String: ชื่อผู้เล่น
+     */
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    /**
+     * @return int: ว่าผู้เล่นมีจำนวนชนะกี่รอบ
+     */
+    public int getGameWin() {
+        return gameWin;
+    }
+
+    /**
+     * @return int: จำนวน
+     */
+    public int getGameLose() {
+        return gamePlayed - gameWin;
+    }
+
+    /**
+     * @return int: จำนวนรอบที่เราเล่นทั้งหมด
+     */
+    public int getGamePlayed() {
+        return gamePlayed;
     }
 }
