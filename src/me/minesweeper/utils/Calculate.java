8bf53c8511d764af8f9eefe3e5a25b169f8f09c7
AloneSpace <jomcootle@gmail.com>
2020-11-19 00:57:14 +0700

    - เพิ่ม calPickupSafePositionPercentage() เพื่อคำนวณค่าเฉลี่ยแต่ละครั้งว่าผู้เล่นเลือกตำแหน่งที่ปลอดภัย - เพิ่ม calWinPercentage() เพื่อคำนวณค่าเฉลี่ยการชนะของผู้เล่นแต่ละคน - เพิ่ม calLosePercentage() เพื่อคำนวณค่าเฉลี่ยการแพ้ของผู้เล่นแต่ละคน

--- src/me/minesweeper/utils/Calculate.java
+++ src/me/minesweeper/utils/Calculate.java
@@ -2,16 +2,30 @@ package me.minesweeper.utils;
 
 public class Calculate {
 
-    public Calculate() {
-
+    /**
+     * @param pickup จำนวนครั้งทั้งหมดที่ผู้เล่นกด
+     * @param bombPickup จำนวนครั้งที่ผู้เล่นกดโดนระเบิด
+     * @return double: Percentage ค่าเฉลี่ยจำนวนที่กดถูก
+     */
+    public double calPickupSafePositionPercentage(int pickup, int bombPickup) {
+        return ((pickup - bombPickup) / (double) pickup) * 100;
     }
 
-    public double calPercentage(int gamePlayed, int gameWon) {
-//        try {
-//
-//        } catch (Exception e) {
-//            System.out.println(e);
-//        }
-        return 0;
+    /**
+     * @param gamePlayed จำนวนรอบที่เล่นทั้งหมด
+      * @param gameWin จำนวนรอบที่ชนะ
+     * @return double: Percentage ค่าเฉลี่ยจำนวนที่ผู้เล่นชนะ
+     */
+    public double calWinPercentage(int gamePlayed, int gameWin) {
+        return (double) (gameWin / gamePlayed) * 100;
+    }
+
+    /**
+     * @param gamePlayed จำนวนรอบที่เล่นทั้งหมด
+     * @param gameLose จำนวนรอบที่แพ้
+     * @return double: Percentage ค่าเฉลี่ยจำนวนที่ผู้เล่นแพ้
+     */
+    public double calLosePercentage(int gamePlayed, int gameLose) {
+        return (double) (gameLose / gamePlayed) * 100;
     }
 }
